a8ee063944e5eeeb1cc40ca8a4a3469f3c9d098e
Kirk Larkin <6025110+serpent5@users.noreply.github.com>
2022-02-09 10:42:08 +0000

    Add busy indicator and extra styles

--- Frontend/src/Root.jsx
+++ Frontend/src/Root.jsx
@@ -28,6 +28,8 @@ export class Root extends React.Component {
   }
 
   render() {
+    const { searchText, isSearching, xrefSuggestions } = this.state;
+
     return (
       <>
         <main className="max-w-screen-xl lg:mx-auto my-12 mx-4">
@@ -35,34 +37,53 @@ export class Root extends React.Component {
             <div className="relative">
               <input
                 type="text"
-                value={this.state.searchText}
+                value={searchText}
                 className="w-full px-14 py-4 rounded-lg"
                 autoFocus
                 onChange={e => this.setState({ searchText: e.target.value })}
+                readOnly={isSearching}
               />
 
               <div className="absolute top-1/2 -translate-y-1/2 left-4 text-gray-500 pointer-events-none">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-8"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
+                {isSearching ? (
+                  <svg className="animate-spin w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-8"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                  </svg>
+                )}
               </div>
             </div>
           </form>
 
-          {this.state.xrefSuggestions?.length > 0 && (
+          {xrefSuggestions?.length > 0 && (
             <ol className="mt-8 bg-white border border-gray-500 divide-y divide-gray-500 rounded-md">
-              {this.state.xrefSuggestions.map(x => (
+              {xrefSuggestions.map(x => (
                 <li key={x}>
                   <Suggestion xrefValue={x} />
                 </li>
@@ -70,7 +91,11 @@ export class Root extends React.Component {
             </ol>
           )}
 
-          {!this.state.isSearching && this.state.xrefSuggestions?.length === 0 && <p>No Results</p>}
+          {!isSearching && xrefSuggestions?.length === 0 && (
+            <div className="mt-8 bg-white border border-gray-500 rounded-md">
+              <p className="px-8 py-6">No Results</p>
+            </div>
+          )}
         </main>
       </>
     );
--- Frontend/src/Suggestion.jsx
+++ Frontend/src/Suggestion.jsx
@@ -7,21 +7,21 @@ export function Suggestion({ xrefValue }) {
         <div className="mt-4 space-x-2">
           <span class="mr-2">Copy:</span>
           <button
-            className="border px-4 py-2 border-gray-400 shadow-sm"
+            className="px-4 py-2 border-2 bg-gray-200 rounded-md shadow-sm hover:bg-gray-400 transition-colors"
             onClick={() => navigator.clipboard.writeText(`<xref:${xrefValue}>`)}
           >
             None
           </button>
 
           <button
-            className="border px-4 py-2 border-gray-400 shadow-sm"
+            className="px-4 py-2 border-2 bg-gray-200 rounded-md shadow-sm hover:bg-gray-400 transition-colors"
             onClick={() => navigator.clipboard.writeText(`<xref:${xrefValue}?displayProperty=fullName>`)}
           >
             Full Name
           </button>
 
           <button
-            className="border px-4 py-2 border-gray-400 shadow-sm"
+            className="px-4 py-2 border-2 bg-gray-200 rounded-md shadow-sm hover:bg-gray-400 transition-colors"
             onClick={() => navigator.clipboard.writeText(`<xref:${xrefValue}?displayProperty=nameWithType>`)}
           >
             Name with Type
